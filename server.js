1383d447f2ef1d8834dd0762d8c7d0b06ab074d4
Hugo Lu <50201563+HugoLu88@users.noreply.github.com>
2021-07-06 18:13:51 +0100

    Corrected mongo link and added cchecks

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken')
 
 const mongousr = "HugoLu88"
 const mongopw = 'Ilovemongo1'
-const mongourl = 'mongodb://localhost:27017/test'
+const mongourl = 'mongodb://localhost:27017/login-db'
 
 const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 const SERVER = 9999
@@ -27,37 +27,53 @@ db.on('error', err => {
   console.error('connection error:', err)
 })
 
-// const app = express()
+const app = express()
 
-// app.use('/', express.static(path.join(__dirname, 'static')))
-// app.use(bodyParser.json()) // middleware to parse a json
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json()) // middleware to parse a json
 
-// app.post('/api/register', async(req, res) => { //request response
-// 	console.log(req.body)
-// 	const {username, password: plaintextpword} = req.body // this is destructuring; taking usernmae and password from the req.body
+app.post('/api/register', async(req, res) => { //request response
+	console.log(req.body)
+	const {username, password: plaintextpword} = req.body // this is destructuring; taking usernmae and password from the req.body
 
-// 	//console.log(await bcrypt.hash(plaintextpword,10))
-// 	const password = await bcrypt.hash(plaintextpword,10)
-// 	try{
-// 		const response = await User.create({
-// 			username,
-// 			password
-// 		})
-// 		console.log("user created")
-// 		console.log(response)
 
-// 	} catch(error) {
-// 		console.log(error.message)
-// 		return res.json({status:"error"})
-// 	}
+	if (!username || typeof username  !== 'string'){
+		return res.json({status:"error", error: 'invalid format'})
+	}
+	if (!plaintextpword || typeof plaintextpword  !== 'string'){
+		return res.json({status:"error", error: 'invalid format of pw'})
+	}
+	if (plaintextpword.length<10){
+		return res.json({status:"error", error: 'pw too short'})
+	}
+	
+	//console.log(await bcrypt.hash(plaintextpword,10))
+	const password = await bcrypt.hash(plaintextpword,10)
+	try{
+		const response = await User.create({
+			username,
+			password
+		})
+		console.log("user created")
+		console.log(response)
+
+	} catch(error) {
+		console.log(error.message)
+		if (error.code === 11000) {
+			console.log("Duplicate key")
+			return res.json({status:"error", error:"duplicate username"})
+		}
+		throw error
+		
+	}
 
 	
-// 	res.json({status:"OK"})
+	res.json({status:"OK"})
 
-// })
+})
 
 
 
-// app.listen(SERVER, () => {
-// 	console.log('Server up at ',SERVER )
-// })
+app.listen(SERVER, () => {
+	console.log('Server up at ',SERVER )
+})
