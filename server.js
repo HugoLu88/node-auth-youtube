cfd4fadb0533c1934668dc0167078c5fa447892d
Hugo Lu <50201563+HugoLu88@users.noreply.github.com>
2021-07-06 16:48:28 +0100

    Comments

--- server.js
+++ server.js
@@ -6,50 +6,58 @@ const User = require('./model/user') // import the user
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+const mongousr = "HugoLu88"
+const mongopw = 'Ilovemongo1'
+const mongourl = 'mongodb://localhost:27017/test'
+
 const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
-const SERVER = 8080
-// mongoose.connect('mongodb://localhost:27012/login-app-db', { //where loginappdb is the name of the databse. 27017 is the mongoose default
-//  	useNewUrlParser: true, // parameters
-//  	useUnifiedTopology: true, // parameters
-//  	useCreateIndex: true // parameters
-//  })
+const SERVER = 9999
+mongoose.connect(mongourl ,{ //where loginappdb is the name of the databse. 27017 is the mongoose default
+  	useNewUrlParser: true, // parameters
+  	useUnifiedTopology: true, // parameters
+  	useCreateIndex: true // parameters
+  })
 
-const app = express()
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', mongourl)
+})
 
-app.use('/', express.static(path.join(__dirname, 'static')))
-app.use(bodyParser.json()) // middleware to parse a json
+db.on('error', err => {
+  console.error('connection error:', err)
+})
 
-app.post('/api/register', async(req, res) => { //request response
-	console.log(req.body)
-	const {username, password: plaintextpword} = req.body // this is destructuring; taking usernmae and password from the req.body
+// const app = express()
 
-	//console.log(await bcrypt.hash(plaintextpword,10))
-	const password = await bcrypt.hash(plaintextpword,10)
-	try{
-		const response = await User.create({
-			username,
-			password
-		})
-		console.log("user created")
-		console.log(response)
+// app.use('/', express.static(path.join(__dirname, 'static')))
+// app.use(bodyParser.json()) // middleware to parse a json
 
-	} catch(error) {
-		console.log(error.message)
-		return res.json({status:"error"})
-	}
+// app.post('/api/register', async(req, res) => { //request response
+// 	console.log(req.body)
+// 	const {username, password: plaintextpword} = req.body // this is destructuring; taking usernmae and password from the req.body
+
+// 	//console.log(await bcrypt.hash(plaintextpword,10))
+// 	const password = await bcrypt.hash(plaintextpword,10)
+// 	try{
+// 		const response = await User.create({
+// 			username,
+// 			password
+// 		})
+// 		console.log("user created")
+// 		console.log(response)
+
+// 	} catch(error) {
+// 		console.log(error.message)
+// 		return res.json({status:"error"})
+// 	}
 
 	
-	res.json({status:"OK"})
+// 	res.json({status:"OK"})
 
-})
-
-
+// })
 
 
 
-
-
-
-app.listen(SERVER, () => {
-	console.log('Server up at ',SERVER )
-})
+// app.listen(SERVER, () => {
+// 	console.log('Server up at ',SERVER )
+// })
